ad5bd5cc506ab86a954828d02076af56d692e4ee
fernandoFlorida <feloba@floridauniversitaria.es>
2024-02-06 12:25:14 +0100

    comprobando sobrevive ataque

--- test/enanaTest.php
+++ test/enanaTest.php
@@ -19,25 +19,37 @@ class enanaTest extends TestCase {
     public function testHeridaLeveVive() {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida suficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es mayor que 0 y además que su situación es viva
-
+        $enana1 = new Enana("ENANA VIVA",100);
+        $comprobacion = false;
+        $this->assertEquals(($enana1->getSituacion()),"viva");
+        if($enana1->getPuntosVida()>0){
+            $comprobacion = true;
+        }
+        $this->assertEquals($comprobacion,true);
     }
 
     public function testHeridaLeveMuere() {
         #Se probará el efecto de una herida leve a una Enana con puntos de vida insuficientes para sobrevivir al ataque
         #Se tendrá que probar que la vida es menor que 0 y además que su situación es muerta
-
+        $enana1 = new Enana("ENANA VIVA",100);
+        $enana2 = new Enana("ENANA LIMBO",0);
+        $enana3 = new Enana("ENANA MUERTA",-32);
     }
 
     public function testHeridaGrave() {
         #Se probará el efecto de una herida grave a una Enana con una situación de viva.
         #Se tendrá que probar que la vida es 0 y además que su situación es limbo
-
+        $enana1 = new Enana("ENANA VIVA",100);
+        $enana2 = new Enana("ENANA LIMBO",0);
+        $enana3 = new Enana("ENANA MUERTA",-32);
     }
     
     public function testPocimaRevive() {
         #Se probará el efecto de administrar una pócima a una Enana muerta pero con una vida mayor que -10 y menor que 0
         #Se tendrá que probar que la vida es mayor que 0 y que su situación ha cambiado a viva
-
+        $enana1 = new Enana("ENANA VIVA",100);
+        $enana2 = new Enana("ENANA LIMBO",0);
+        $enana3 = new Enana("ENANA MUERTA",-32);
     }
 
     public function testPocimaNoRevive() {
